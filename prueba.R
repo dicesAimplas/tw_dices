baff5a698e1911e9771b704e0c93573507254051
dicesAimplas <83291321+dicesAimplas@users.noreply.github.com>
2021-07-06 11:56:34 +0200

    Update and rename correo.R to prueba.R

---
+++ prueba.R
@@ -0,0 +1,11 @@
+library(rtweet)
+
+twitter_token <- create_token(
+  app = "AIMPLAS",
+  consumer_key = '1XGKl9YCewt9bb9gSPB2XCllu',
+  consumer_secret = '4SheB2UP5Iwj1ZgFETmULdEUvkHtTYha9M6CtG2TFl78sPxFfd',
+  access_token = '525164307-GbJrPL3uvrVvPEJ8zvcBa49SKkj1zfzxOJnNppAR',
+  access_secret = 'OWjar31m93kHcYAzHFvHfJQo3p0l2y1jQVbPcyOybtt13'
+)
+
+data <- get_timeline("aimplas", n = 10000)
