0f6bfbc15bac0245d3f1f9bfbdad5a17d3f4573d
dicesAimplas <83291321+dicesAimplas@users.noreply.github.com>
2021-07-19 09:40:10 +0200

    Create tw_update_weekly.R

---
+++ tw_update_weekly.R
@@ -0,0 +1,23 @@
+source("src/fun_auxiliar.R")
+source("src/functions.R")
+
+packages <- c("rtweet", "tidyverse", "visNetwork", "tm", "RJSONIO", "RCurl", "syuzhet", "lubridate", "xts")
+invisible(lapply(packages, library, character.only = TRUE))
+
+twitter_token <- create_token(
+  app = "AIMPLAS",
+  consumer_key = '1XGKl9YCewt9bb9gSPB2XCllu',
+  consumer_secret = '4SheB2UP5Iwj1ZgFETmULdEUvkHtTYha9M6CtG2TFl78sPxFfd',
+  access_token = '525164307-GbJrPL3uvrVvPEJ8zvcBa49SKkj1zfzxOJnNppAR',
+  access_secret = 'OWjar31m93kHcYAzHFvHfJQo3p0l2y1jQVbPcyOybtt13'
+)
+
+
+main <- function() {
+  #Seguidores
+  Followers_Aimplas()
+}
+
+if(getOption("run.main", default=TRUE)) {
+  main()
+}
