e25cde5a38570e206e90b7921250b1a4a7a0907f
dicesAimplas <83291321+dicesAimplas@users.noreply.github.com>
2021-09-07 12:41:51 +0200

    Add files via upload

---
+++ call.R
@@ -0,0 +1,43 @@
+source("fun_auxiliar.R")
+source("functions.R")
+
+#actualización semanal
+
+packages <- c("rtweet", "tidyverse", "visNetwork", "tm", "RJSONIO", "RCurl", "syuzhet", "lubridate", "xts", "readr")
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
+
+#tokens de twitter necesarios para la libreria rtweet
+twitter_token <- create_token(
+  app = "AIMPLAS",
+  consumer_key = '1XGKl9YCewt9bb9gSPB2XCllu',
+  consumer_secret = '4SheB2UP5Iwj1ZgFETmULdEUvkHtTYha9M6CtG2TFl78sPxFfd',
+  access_token = '525164307-GbJrPL3uvrVvPEJ8zvcBa49SKkj1zfzxOJnNppAR',
+  access_secret = 'OWjar31m93kHcYAzHFvHfJQo3p0l2y1jQVbPcyOybtt13'
+)
+
+
+main <- function() {
+  #recogida de tweets en los que aparece "aimplas"
+  Tweets_Aimplas()
+  
+  #recogida de tweets en los que aparece "plastico"
+  Tweets_Plasticos()
+-
+  #actualización seguidores aimplas
+  #Followers_Aimplas()
+  
+  #analisis de sentimientos
+  Plasticos_Sentimientos_syuzhet()
+}
+
+if(getOption("run.main", default=TRUE)) {
+  main()
+}
